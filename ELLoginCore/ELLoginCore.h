7d5f54b896d23e5ffe968b56ad05b45b461ce030
Yevhenii Lytvynenko <litvinenko.evg@gmail.com>
2018-12-23 19:40:00 +0200

    project is created

---
+++ ELLoginCore/ELLoginCore.h
@@ -0,0 +1,19 @@
+//
+//  ELLoginCore.h
+//  ELLoginCore
+//
+//  Created by Yevhenii Lytvynenko on 12/23/18.
+//  Copyright © 2018 Yevhenii Lytvynenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ELLoginCore.
+FOUNDATION_EXPORT double ELLoginCoreVersionNumber;
+
+//! Project version string for ELLoginCore.
+FOUNDATION_EXPORT const unsigned char ELLoginCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELLoginCore/PublicHeader.h>
+
+
